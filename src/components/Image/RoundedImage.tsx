dce9c0c3f63ffacca065d9437980cf46ad0c4b00
poocart <as.deimis@gmail.com>
2022-11-17 19:28:57 +0200

    bridge quote option finalized

--- src/components/Image/RoundedImage.tsx
+++ src/components/Image/RoundedImage.tsx
@@ -3,14 +3,23 @@ import React, {
 } from 'react';
 import styled from 'styled-components';
 
-const Image = styled.img<{ size?: number, noMarginRight?: boolean }>`
+const Image = styled.img<{
+  size?: number,
+  noMarginRight?: boolean;
+  marginTop?: number;
+}>`
   height: ${({ size }) => size ?? 32}px;
   width: ${({ size }) => size ?? 32}px;
   border-radius: 50%;
   ${({ noMarginRight }) => !noMarginRight && `margin-right: 11px`};
+  ${({ marginTop }) => marginTop && `margin-top: ${marginTop}px`};
 `;
 
-const FallbackImage = styled.div<{ size?: number, noMarginRight?: boolean }>`
+const FallbackImage = styled.div<{
+  size?: number;
+  noMarginRight?: boolean;
+  marginTop?: number;
+}>`
   font-family: "PTRootUIWebBold", sans-serif;
   font-size: ${({ size }) => size ? size / 2 : 16}px;
   line-height: ${({ size }) => size ?? 32}px;
@@ -22,6 +31,7 @@ const FallbackImage = styled.div<{ size?: number, noMarginRight?: boolean }>`
   color: ${({ theme }) => theme.color.text.roundedImageFallback};
   text-align: center;
   text-transform: uppercase;
+  ${({ marginTop }) => marginTop && `margin-top: ${marginTop}px`};
 `;
 
 const RoundedImage = ({
@@ -29,16 +39,26 @@ const RoundedImage = ({
   url,
   size,
   noMarginRight,
+  marginTop,
 }: {
   title: string,
   url: string | undefined,
   size?: number,
   noMarginRight?: boolean,
+  marginTop?: number,
 })=> {
   const [useFallback, setUseFallback] = useState(false);
 
   if (useFallback || !url) {
-    return <FallbackImage size={size} noMarginRight={noMarginRight}>{title[0]}</FallbackImage>;
+    return (
+      <FallbackImage
+        size={size}
+        noMarginRight={noMarginRight}
+        marginTop={marginTop}
+      >
+        {title[0]}
+      </FallbackImage>
+    );
   }
 
   return (
@@ -47,6 +67,7 @@ const RoundedImage = ({
       src={url}
       alt={title}
       noMarginRight={noMarginRight}
+      marginTop={marginTop}
       onError={({ currentTarget }) => {
         currentTarget.onerror = null;
         setUseFallback(true);
