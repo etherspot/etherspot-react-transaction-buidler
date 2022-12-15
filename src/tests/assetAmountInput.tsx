9c2c51c12f0ee0f95a14c45cd96936ac08eaf285
Parag Chauhan <parag.chauhan@bacancy.com>
2022-12-15 18:01:24 +0530

    Added Jest test cases for formatAssetAmountInput

---
+++ src/tests/assetAmountInput.tsx
@@ -0,0 +1,13 @@
+import { formatAssetAmountInput } from '../utils/common';
+
+describe('formatAmount', () => {
+  it('should format transaction amount to replace leading dot with 0.', () => {
+    expect(formatAssetAmountInput('.')).toBe('0.');
+  });
+  it('should format transaction amount to replace leading dot with 0.', () => {
+    expect(formatAssetAmountInput('0.1')).toBe('0.1');
+  });
+  it('should format transaction amount to replace leading dot with 0.', () => {
+    expect(formatAssetAmountInput('1')).toBe('1');
+  });
+});
