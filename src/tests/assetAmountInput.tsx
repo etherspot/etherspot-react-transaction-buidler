d7dbc3bdde0523fc11b1fef9eec6d946a8d2bc5c
Parag Chauhan <parag.chauhan@bacancy.com>
2022-12-15 18:07:21 +0530

    Change description in asset amount input test case.

--- src/tests/assetAmountInput.tsx
+++ src/tests/assetAmountInput.tsx
@@ -4,10 +4,10 @@ describe('formatAmount', () => {
   it('should format transaction amount to replace leading dot with 0.', () => {
     expect(formatAssetAmountInput('.')).toBe('0.');
   });
-  it('should format transaction amount to replace leading dot with 0.', () => {
+  it('should format transaction amount to allow decimal numbers', () => {
     expect(formatAssetAmountInput('0.1')).toBe('0.1');
   });
-  it('should format transaction amount to replace leading dot with 0.', () => {
+  it('should format transaction amount to allow Non-negative integers', () => {
     expect(formatAssetAmountInput('1')).toBe('1');
   });
 });
