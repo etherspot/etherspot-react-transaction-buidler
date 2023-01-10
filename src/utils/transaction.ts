c9b949cb5268c5bc3435d72efa59bd618a24ceb6
vignesha22 <vignesh@pillarproject.io>
2023-01-11 02:48:18 +0530

    added fromAddress and removed check against current chain on submitting key based tx

--- src/utils/transaction.ts
+++ src/utils/transaction.ts
@@ -770,10 +770,9 @@ export const submitWeb3ProviderTransactions = async (
   }
 
   // TODO: check against current
-  if (chainId !== 1) {
-    const changed = await changeToChain(chainId);
-    if (!changed) return { errorMessage: 'Unable to change to selected network!' };
-  }
+  // Even if its on same chain it returns correctly so that we dont have to check against current chainId
+  const changed = await changeToChain(chainId);
+  if (!changed) return { errorMessage: 'Unable to change to selected network!' };
 
   try {
     for (const transaction of transactions) {
