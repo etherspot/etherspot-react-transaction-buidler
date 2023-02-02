024d7287421491e3eb053413a7e2d108e248c9e1
Parag Chauhan <parag.chauhan@bacancy.com>
2023-02-02 14:28:26 +0530

    Fix alignment issue

--- src/providers/TransactionBuilderContextProvider.tsx
+++ src/providers/TransactionBuilderContextProvider.tsx
@@ -162,6 +162,9 @@ const MenuWrapper = styled.div`
 `;
 
 const MenuItem = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
   margin-bottom: 10px;
   cursor: pointer;
 
