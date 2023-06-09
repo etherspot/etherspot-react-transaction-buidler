282780d1d09c68d8cb7239c1c678d447043844c4
Yash-Chaubal <yashschaubal@gmail.com>
2023-06-09 17:58:23 +0100

    added disable input prop

--- src/components/TransactionBlock/SendAssetTransactionBlock.tsx
+++ src/components/TransactionBlock/SendAssetTransactionBlock.tsx
@@ -46,6 +46,9 @@ const SendAssetTransactionBlock = ({
   const [amount, setAmount] = useState<string>(values?.amount ?? '');
   const [selectedAsset, setSelectedAsset] = useState<IAssetWithBalance | null>(values?.selectedAsset ?? null);
   const [selectedNetwork, setSelectedNetwork] = useState<Chain | null>(values?.chain ?? null);
+  const [disableReceiverAddressInput, setDisableReceiverAddressInput] = useState<boolean>(
+    values?.disableReceiverAddressInput ?? false
+  );
   const [isFromEtherspotWallet, setIsFromEtherspotWallet] = useState<boolean>(values?.isFromEtherspotWallet ?? true);
   const [selectedAccountType, setSelectedAccountType] = useState<string>(
     values?.isFromEtherspotWallet ?? true ? AccountTypes.Contract : AccountTypes.Key
@@ -112,6 +115,7 @@ const SendAssetTransactionBlock = ({
         isFromEtherspotWallet,
         fromAddress: (isFromEtherspotWallet ? accountAddress : providerAddress) as string,
         accountType: selectedAccountType,
+        disableReceiverAddressInput,
       },
       multiCallData || undefined
     );
@@ -215,7 +219,7 @@ const SendAssetTransactionBlock = ({
         displayLabelOutside
         smallerInput
         showPasteButton
-        disabled={!!fixed || values?.disableReceiverAddressInput}
+        disabled={!!fixed || disableReceiverAddressInput}
       />
     </>
   );
