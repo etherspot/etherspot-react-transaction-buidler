df1c6b648f8a6abe0bddfeb3bce68cb0f91da228
MayukhSamadder2000 <mayukhmmm@gmail.com>
2023-06-12 18:58:47 +0530

    fix: token approval

--- src/utils/transaction.ts
+++ src/utils/transaction.ts
@@ -534,9 +534,9 @@ export const honeyswapLP = async (
 
     const ERC20Intance = new ethers.utils.Interface(approveAbi);
 
-    const approvalData1 = ERC20Intance.encodeFunctionData('approve', [contractAddressProxy, amount1]);
+    const approvalData1 = ERC20Intance.encodeFunctionData('approve', [contractAddress, amount1]);
 
-    const approvalData2 = ERC20Intance.encodeFunctionData('approve', [contractAddressProxy, amount2]);
+    const approvalData2 = ERC20Intance.encodeFunctionData('approve', [contractAddress, amount2]);
 
     const newEncodeAddLiquidityTransactions = {
       to: contractAddress,
